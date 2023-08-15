703fb73cb75d874bc59de5c37c2ce0d583a068d0
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-15 15:28:06 +0800

    feat: should_return_multiplication_expression_when_multiply_given_multiplicand_and_multiplier

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -26,6 +26,6 @@ public class MultiplicationTable {
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand + "*" + multiplier + "=" + (multiplicand * multiplier);
     }
 }
